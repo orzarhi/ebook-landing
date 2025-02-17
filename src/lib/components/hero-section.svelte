7eae867b3b161fbf25e5c5a3dbade2155cd544f0
orzarhi <or.zarhi11@gmail.com>
2025-02-17 10:01:31 +0200

    first commit

---
+++ src/lib/components/hero-section.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import bookCover from '$assets/book_cover.png';
+</script>
+
+<section class="hero">
+	<div class="hero-showcase text-center bold">
+		<h1 class="purple">MOVE TO</h1>
+		<h1 class="purple mb-xs">SPAIN</h1>
+		<h4 class="italic semi-bold">There's no need to make the same mistakes i made.</h4>
+        <img src={bookCover} alt="Book Cover" class="book-cover" />
+	</div>
+</section>
