252168c358adfe5f5f90222c792ef20ba858471f
orzarhi <or.zarhi11@gmail.com>
2025-02-24 11:12:33 +0200

    chore: Add Stripe checkout functionality

--- src/lib/components/button.svelte
+++ src/lib/components/button.svelte
@@ -1,15 +1,41 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 	import type { HTMLAttributes } from 'svelte/elements';
+	import { loadStripe } from '@stripe/stripe-js';
+	import { PUBLIC_STRIPE_KEY } from '$env/static/public';
+	import { goto } from '$app/navigation';
 
 	interface Props extends HTMLAttributes<HTMLButtonElement> {
 		children: Snippet;
 	}
 
 	const { children, ...props }: Props = $props();
+
+	async function onclick() {
+		try {
+			const stripe = await loadStripe(PUBLIC_STRIPE_KEY);
+
+			if (!stripe) {
+				throw Error('Failed to load Stripe');
+			}
+
+			const response = await fetch('/api/checkout', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+
+			const { sessionId } = await response.json();
+
+			await stripe.redirectToCheckout({ sessionId });
+		} catch (error) {
+			goto('/checkout/failure');
+		}
+	}
 </script>
 
-<button {...props}>{@render children()}</button>
+<button {onclick} {...props}>{@render children()}</button>
 
 <style>
 	button {
