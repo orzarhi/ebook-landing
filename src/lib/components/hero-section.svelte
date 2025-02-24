e959d54ced0882e01308f463eb1ab35b667ecf98
orzarhi <or.zarhi11@gmail.com>
2025-02-24 11:37:22 +0200

    chore: Update hero section to conditionally render children

--- src/lib/components/hero-section.svelte
+++ src/lib/components/hero-section.svelte
@@ -2,6 +2,8 @@
 	import bookCover from '$assets/book_cover.png';
 	import phoneCover from '$assets/phone_cover.png';
 	import { Button } from '$components';
+
+	let { children } = $props();
 </script>
 
 <section class="hero">
@@ -13,15 +15,20 @@
 		<img src={phoneCover} alt="Phone Cover" class="phone-cover" />
 	</div>
 	<div class="hero-text white text-center">
-		<h1>THIS EBOOK WILL</h1>
-		<h1>SAVE YOU MONEY,</h1>
-		<h1 class="mb-m">TIME & NERVES</h1>
-		<p class="light-grey mb-l">
-			I moved to Spain seven years ago. The process was painful and difficult. I learned most things
-			the hard way: finding a job, getting a visa, calculating my cost of living, choosing an
-			apartment. That's why I wrote this e-book, to tell you how I did it. No bullshit, no stress.
-		</p>
-		<Button onclick={() => console.log('hi')}>Purchase E-book for $10</Button>
+		{#if children}
+			{@render children()}
+		{:else}
+			<h1>THIS EBOOK WILL</h1>
+			<h1>SAVE YOU MONEY,</h1>
+			<h1 class="mb-m">TIME & NERVES</h1>
+			<p class="light-grey mb-l">
+				I moved to Spain seven years ago. The process was painful and difficult. I learned most
+				things the hard way: finding a job, getting a visa, calculating my cost of living, choosing
+				an apartment. That's why I wrote this e-book, to tell you how I did it. No bullshit, no
+				stress.
+			</p>
+			<Button onclick={() => console.log('hi')}>Purchase E-book for $10</Button>
+		{/if}
 	</div>
 </section>
 
